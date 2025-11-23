7211112fa409201d690e35b70445b2603b031d00
blaszka0 <mikuwu31@gmail.com>
2025-11-23 19:08:45 +0000

    Dodano logowanie

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import {resolve} from "$app/paths";
+	import LoginForm from "../../lib/client/core/authentication/LoginForm.svelte";
+</script>
+
+<LoginForm urlOfSubmission={resolve("/login")} />
