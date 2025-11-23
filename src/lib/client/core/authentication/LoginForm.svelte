93566651bf7e09a95d37c0563f3191e5c0023ed3
blaszka0 <mikuwu31@gmail.com>
2025-11-23 19:08:06 +0000

    Dodano formularz logowania

---
+++ src/lib/client/core/authentication/LoginForm.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	const props: {readonly urlOfSubmission: string} = $props();
+</script>
+
+<form action={props.urlOfSubmission} method="POST">
+	<input type="email" placeholder="Adres E-mail" />
+	<button type="submit">Kontynuuj</button>
+</form>
