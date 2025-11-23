19758b8a577c42c9f2973bbb5992cf7570a57875
blaszka0 <mikuwu31@gmail.com>
2025-11-23 19:05:53 +0000

    Dodano wyświetlanie paska nawigacji

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import type {Snippet} from "svelte";
 	import ContainerOfMainContent from "../lib/client/core/container-of-main-content/ContainerOfMainContent.svelte";
+	import NavigationBar from "../lib/client/core/navigation-bar/NavigationBar.svelte";
 	const props: {readonly children: Snippet<[]>} = $props();
 </script>
 
+<NavigationBar />
 <ContainerOfMainContent children={props.children} />
