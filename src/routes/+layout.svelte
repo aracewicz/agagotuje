3a5b3601eb95d6a5af8e4e259b9b8784dd642153
blaszka0 <mikuwu31@gmail.com>
2025-11-23 20:13:53 +0000

    Dodano wyświetlanie kontenera głównej zawartości

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type {Snippet} from "svelte";
+	import ContainerOfMainContent from "../lib/client/core/container-of-main-content/ContainerOfMainContent.svelte";
 	const props: {readonly children: Snippet<[]>} = $props();
 </script>
 
-{@render props.children()}
+<ContainerOfMainContent children={props.children} />
