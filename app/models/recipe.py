5da3a7b28de331c900b35cbce9c5f7f9ed90a71d
BlazejMazurek <144492398+BlazejMazurek@users.noreply.github.com>
2025-11-29 22:02:18 +0100

    Update recipe.py

--- app/models/recipe.py
+++ app/models/recipe.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Text, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import JSONB
 from app.core.database import Base
 
 class Recipe(Base):
@@ -8,7 +9,7 @@ class Recipe(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, nullable=False)
-    ingredients = Column(String, nullable=False)
+    ingredients = Column(JSONB, nullable=False)
     description = Column(Text, nullable=False)
     author_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     image_url = Column(String, nullable=True)
