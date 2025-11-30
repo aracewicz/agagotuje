d27885991aa28f5be7b7e6020577b9a16cd05a69
blaszka0 <mikuwu31@gmail.com>
2025-11-30 20:21:17 +0000

    połączenie z backendem

--- src/lib/server/core/config/schema/schemaOfConfig.ts
+++ src/lib/server/core/config/schema/schemaOfConfig.ts
@@ -1,5 +1,5 @@
 import {z} from "zod";
 import type {Config} from "../Config.ts";
 export const schemaOfConfig = z.object({}).transform((env): Config => {
-	return {api: {baseUrl: "http://172.17.0.1:8000"}};
+	return {api: {baseUrl: "http://localhost:8000"}};
 });
